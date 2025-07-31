5b93cd5e45f48467ef9e062ee80b6c1dfbb81447
tariq615 <iqbaltariq615@gmail.com>
2025-08-01 00:11:06 +0300

    docker added

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,21 +1,76 @@
 import { stripe } from "../app.js";
 import { TryCatch } from "../middlewares/error.js";
 import { couponModel } from "../models/coupon.model.js";
+import { productModel } from "../models/product.model.js";
+import { userModel } from "../models/user.model.js";
+import { orderItemsType, shippingInfoType } from "../types/types.js";
 import ErrorHandler from "../utils/utility-class.js";
 
 const createPaymentIntent = TryCatch(async (req, res, next) => {
-  const { amount } = req.body;
+   const { id } = req.query;
 
-  if (!amount) {
-    return next(new ErrorHandler("Please enter amount ", 400));
+  const user = await userModel.findById(id).select("name");
+
+  if (!user) return next(new ErrorHandler("Please login first", 401));
+
+  const {
+    items,
+    shippingInfo,
+    coupon,
+  }: {
+    items: orderItemsType[];
+    shippingInfo: shippingInfoType | undefined;
+    coupon: string | undefined;
+  } = req.body;
+
+  if (!items) return next(new ErrorHandler("Please send items", 400));
+
+  if (!shippingInfo)
+    return next(new ErrorHandler("Please send shipping info", 400));
+
+  let discountAmount = 0;
+
+  if (coupon) {
+    const discount = await couponModel.findOne({ code: coupon });
+    if (!discount) return next(new ErrorHandler("Invalid Coupon Code", 400));
+    discountAmount = discount.amount;
   }
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: Number(amount) * 100,
-    currency: "pkr"
-  })
+  const productIDs = items.map((item) => item.productId);
 
-  res.status(201).json({
+  const products = await productModel.find({
+    _id: { $in: productIDs },
+  });
+
+  const subtotal = products.reduce((prev, curr) => {
+    const item = items.find((i) => i.productId === curr._id.toString());
+    if (!item) return prev;
+    return curr.price * item.quantity + prev;
+  }, 0);
+
+  const tax = subtotal * 0.18;
+
+  const shipping = subtotal > 1000 ? 0 : 200;
+
+  const total = Math.floor(subtotal + tax + shipping - discountAmount);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total * 100,
+    currency: "pkr",
+    description: "Bin Iqbal's store",
+    shipping: {
+      name: user.name,
+      address: {
+        line1: shippingInfo.address,
+        postal_code: shippingInfo.pinCode.toString(),
+        city: shippingInfo.city,
+        state: shippingInfo.state,
+        country: shippingInfo.country,
+      },
+    },
+  });
+
+  return res.status(201).json({
     success: true,
     clientSecret: paymentIntent.client_secret,
   });
